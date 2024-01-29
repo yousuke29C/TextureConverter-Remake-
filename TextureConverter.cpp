7239cf86e53aef101f98215f86a5547ac6666ee6
yousuke29C <k021g1384@g.neec.ac.jp>
2024-01-30 01:22:58 +0900

    2_5

--- TextureConverter.cpp
+++ TextureConverter.cpp
@@ -1,23 +1,98 @@
 #include "TextureConverter.h"
 
+using namespace DirectX;
+HRESULT result;
+
 void TextureConverter::ConvertTextureWICToDDS(const std::string& filePath)
 {
+
 	LoadWICTextureFromFile(filePath);
+
+	SaveDDSTextureToFile();
+
 }
 
 void TextureConverter::LoadWICTextureFromFile(const std::string& filePath)
 {
+
 	std::wstring wfilePath = ConvertMultiByteStringToWideString(filePath);
+
+	result = LoadFromWICFile(wfilePath.c_str(), WIC_FLAGS_NONE, &metadata_, scratchImage_);
+	assert(SUCCEEDED(result));
+
+	SeparateFilePath(wfilePath);
+
 }
+
+void TextureConverter::SaveDDSTextureToFile()
+{
+	metadata_.format = MakeSRGB(metadata_.format);
+
+	HRESULT result;
+
+	std::wstring filePath = directoryPath_ + fileName_ + L".dds";
+
+	result = SaveToDDSFile(scratchImage_.GetImages(), scratchImage_.GetImageCount(), metadata_, DDS_FLAGS_NONE, filePath.c_str());
+
+	assert(SUCCEEDED(result));
+
+}
+
 std::wstring TextureConverter::ConvertMultiByteStringToWideString(const std::string& mString)
 {
 	int filePathBufferSize = MultiByteToWideChar(CP_ACP, 0, mString.c_str(), -1, nullptr, 0);
 
 	std::wstring wString;
-
 	wString.resize(filePathBufferSize);
 
 	MultiByteToWideChar(CP_ACP, 0, mString.c_str(), -1, &wString[0], filePathBufferSize);
 
 	return wString;
+
+}
+
+void TextureConverter::SeparateFilePath(const std::wstring& filePath)
+{
+	size_t pos1;
+	std::wstring exceptExt;
+
+	pos1 = filePath.rfind('.');
+
+	if (pos1 != std::wstring::npos) {
+
+		fileExt_ = filePath.substr(pos1 + 1, filePath.size() - pos1 - 1);
+		exceptExt = filePath.substr(0, pos1);
+	}
+	else {
+		fileExt_ = L"";
+		exceptExt = filePath;
+
+	}
+
+	pos1 = exceptExt.rfind('\\');
+	if (pos1 != std::wstring::npos) {
+
+		directoryPath_ = exceptExt.substr(0, pos1 + 1);
+
+		fileName_ = exceptExt.substr(pos1 + 1, exceptExt.size() - pos1 - 1);
+
+		return;
+
+	}
+
+	pos1 = exceptExt.rfind('/');
+	if (pos1 != std::wstring::npos) {
+
+		directoryPath_ = exceptExt.substr(0, pos1 + 1);
+
+		fileName_ = exceptExt.substr(pos1 + 1, exceptExt.size() - pos1 - 1);
+
+		return;
+
+	}
+
+	directoryPath_ = L"";
+	fileName_ = exceptExt;
+
+
 }
--- TextureConverter.h
+++ TextureConverter.h
@@ -5,9 +5,28 @@
 class TextureConverter
 {
 public:
+	
 	void ConvertTextureWICToDDS(const std::string& filePath);
 
 private:
+	
+	void SaveDDSTextureToFile();
+
 	void LoadWICTextureFromFile(const std::string& filePath);
+
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+
+	void SeparateFilePath(const std::wstring& filePath);
+
+private:
+	
+	DirectX::TexMetadata metadata_;
+	
+	DirectX::ScratchImage scratchImage_;
+
+	std::wstring directoryPath_;
+	
+	std::wstring fileName_;
+	
+	std::wstring fileExt_;
 };
